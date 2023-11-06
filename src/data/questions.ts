04d5d426ca5b5e088064297fbc67d36919af7330
Emanuelle Sales <39285319+EmanuelleSalesG@users.noreply.github.com>
2023-11-06 17:07:59 -0300

    template: quiz

---
+++ src/data/questions.ts
@@ -0,0 +1,14 @@
+import { Question } from "@/types/Questions";
+
+export const questions: Question[] = [
+    {
+        question: 'What is the correct command to create a new React project?',
+        options: [
+            'npx crate-next-app',
+            'npm create-react-app',
+            'npm create-react-app myReactApp',
+            'npx create-react-app MyReactApp'
+        ],
+        answer: 0
+    }
+];
